ec1ea4e5982db6cd28395285f80e247710fd4e54
Charlie Boggus <charlie.boggus@gmail.com>
2019-05-28 15:02:33 -0400

    Cleaned up mmu.rs

--- src/mmu.rs
+++ src/mmu.rs
@@ -66,20 +66,39 @@ impl MMU
     {
         match addr
         {
-            0x0000...0x7FFF => { /* ROM */ },
-            0x8000...0x9FFF => { return self.gpu.read_byte(addr); },
-            0xA000...0xBFFF => { /* EXT RAM */ },
-            0xC000...0xCFFF => { return self.wram[(addr - 0xC000) as usize]; },
-            0xD000...0xDFFF => { return self.wram[(addr - 0xD000) as usize]; },
-            0xE000...0xFDFF => { return self.wram[(addr - 0xE000) as usize];},
-            0xFE00...0xFE9F => { return self.gpu.read_byte(addr); },
-            0xFEA0...0xFEFF => { return 0x0; }, // Not used
-            0xFF00...0xFF7F => { return self.read_io(addr); },
-            0xFF80...0xFFFE => { return self.hram[(addr - 0xFF80) as usize]; },
-            0xFFFF          => { /* IE Register */ }
-        }
+            // ROM
+            0x0000...0x7FFF => 0x0,
 
-        self.memory[addr as usize]
+            // VRAM
+            0x8000...0x9FFF => self.gpu.read_byte(addr),
+
+            // EXT RAM
+            0xA000...0xBFFF => 0x0,
+
+            // WRAM Bank 0
+            0xC000...0xCFFF => self.wram[(addr - 0xC000) as usize],
+
+            // WRAM Bank 1
+            0xD000...0xDFFF => self.wram[(addr - 0xD000) as usize],
+
+            // WRAM ECHO
+            0xE000...0xFDFF => self.wram[(addr - 0xE000) as usize],
+
+            // OAM
+            0xFE00...0xFE9F => self.gpu.read_byte(addr),
+
+            // Unused
+            0xFEA0...0xFEFF => 0x0,
+
+            // IO Registers
+            0xFF00...0xFF7F => self.read_io(addr),
+
+            // HRAM
+            0xFF80...0xFFFE => self.hram[(addr - 0xFF80) as usize],
+
+            // IE Register
+            0xFFFF          => 0x0
+        }
     }
 
     /// Function to write a byte to memory at the given address
@@ -87,17 +106,38 @@ impl MMU
     {
         match addr
         {
-            0x0000...0x7FFF => { /* ROM */ },
+            // ROM
+            0x0000...0x7FFF => { },
+
+            // VRAM
             0x8000...0x9FFF => { self.gpu.write_byte(addr, val); },
-            0xA000...0xBFFF => { /* EXT RAM */ },
+
+            // EXT RAM
+            0xA000...0xBFFF => { },
+
+            // WRAM Bank 0
             0xC000...0xCFFF => { self.wram[(addr - 0xC000) as usize] = val; },
+
+            // WRAM Bank 1
             0xD000...0xDFFF => { self.wram[(addr - 0xD000) as usize] = val; },
+
+            // WRAM ECHO
             0xE000...0xFDFF => { self.wram[(addr - 0xE000) as usize] = val; },
+
+            // OAM
             0xFE00...0xFE9F => { self.gpu.write_byte(addr, val); },
-            0xFEA0...0xFEFF => { /* Not Used */ },
+
+            // Unused
+            0xFEA0...0xFEFF => { },
+
+            // IO registers
             0xFF00...0xFF7F => { self.write_io(addr, val); },
+
+            // HRAM
             0xFF80...0xFFFE => { self.hram[(addr - 0xFF80) as usize] = val; },
-            0xFFFF          => { /* IE Register */ }
+
+            // IE Register
+            0xFFFF          => { }
         }
 
         // TODO: this is only temporary
@@ -132,146 +172,61 @@ impl MMU
 
     fn read_io(&self, addr: u16) -> u8
     {
-        let offset = addr - 0xFF00;
-        match offset
+        match addr
         {
             // Input
-            0x00 => {  },
+            0xFF00 => {  },
 
             // Serial Data
-            0x01 => {  },
+            0xFF01 => {  },
 
             // Serial Control
-            0x02 => {  },
+            0xFF02 => {  },
 
-            // Timer DIV register
-            0x04 => { return self.timer.read_byte(addr); },
-
-            // Timer TIMA register
-            0x05 => { return self.timer.read_byte(addr); },
-
-            // Timer TMA register
-            0x06 => { return self.timer.read_byte(addr); },
-
-            // Timer TCA register
-            0x07 => { return self.timer.read_byte(addr); },
+            // Timer
+            0xFF04...0xFF07 => { return self.timer.read_byte(addr); }
 
             // Interrupt Flag Register
-            0x0F => {  },
+            0xFF0F => {  },
 
-            // Sound stuff... implement later
-            // TODO: this
-            0x10...0x3F => {  },
+            // SPU
+            // TODO: implement sound later
+            0xFF10...0xFF3F => {  },
 
-            // LCD Control (LCDC) register
-            0x40 => { return self.gpu.read_byte(addr); },
-
-            // LCDC Status + IT selection
-            0x41 => { return self.gpu.read_byte(addr); },
-
-            // LCDC bg Y pos
-            0x42 => { return self.gpu.read_byte(addr); },
-
-            // LCDC bg X pos
-            0x43 => { return self.gpu.read_byte(addr); },
-
-            // Currently displayed line
-            0x44 => { return self.gpu.read_byte(addr); },
-
-            // Current line compare
-            0x45 => { return self.gpu.read_byte(addr); },
-
-            // DMA transfer from ROM/RAM to OAM
-            0x46 => { return self.gpu.read_byte(addr); },
-
-            // Background pallette
-            0x47 => { return self.gpu.read_byte(addr); },
-
-            // Sprite Pallette 0
-            0x48 => { return self.gpu.read_byte(addr); },
-
-            // Sprite Pallette 1
-            0x49 => { return self.gpu.read_byte(addr); },
-
-            // Window Y position
-            0x4A => { return self.gpu.read_byte(addr); },
-
-            // Window X position
-            0x4B => { return self.gpu.read_byte(addr); },
+            // GPU
+            0xFF40...0xFF4B => { return self.gpu.read_byte(addr); }
 
             _ => {  }
         }
+
         0
     }
 
     fn write_io(&mut self, addr: u16, val: u8)
     {
-        let offset = addr - 0xFF00;
-        match offset
+        match addr
         {
             // Input
-            0x00 => {  },
+            0xFF00 => {  },
 
             // Serial Data
-            0x01 => {  },
+            0xFF01 => {  },
 
             // Serial Control
-            0x02 => {  },
+            0xFF02 => {  },
 
-            // Timer DIV register
-            0x04 => { self.timer.write_byte(addr, val); },
-
-            // Timer TIMA register
-            0x05 => { self.timer.write_byte(addr, val); },
-
-            // Timer TMA register
-            0x06 => { self.timer.write_byte(addr, val); },
-
-            // Timer TCA register
-            0x07 => { self.timer.write_byte(addr, val); },
+            // Timer
+            0xFF04...0xFF07 => { self.timer.write_byte(addr, val); }
 
             // Interrupt Flag Register
-            0x0F => {  },
+            0xFF0F => {  },
 
-            // Sound stuff... implement later
+            // SPU... implement sound later
             // TODO: this
-            0x10...0x3F => {  },
+            0xFF10...0xFF3F => {  },
 
-            // LCD Control (LCDC) register
-            0x40 => { self.gpu.write_byte(addr, val); },
-
-            // LCDC Status + IT selection
-            0x41 => { self.gpu.write_byte(addr, val); },
-
-            // LCDC bg Y pos
-            0x42 => { self.gpu.write_byte(addr, val); },
-
-            // LCDC bg X pos
-            0x43 => { self.gpu.write_byte(addr, val); },
-
-            // Currently displayed line
-            0x44 => { self.gpu.write_byte(addr, val); },
-
-            // Current line compare
-            0x45 => { self.gpu.write_byte(addr, val); },
-
-            // DMA transfer from ROM/RAM to OAM
-            0x46 => { self.gpu.write_byte(addr, val); },
-
-            // Background pallette
-            0x47 => { self.gpu.write_byte(addr, val); },
-
-            // Sprite Pallette 0
-            0x48 => { self.gpu.write_byte(addr, val); },
-
-            // Sprite Pallette 1
-            0x49 => { self.gpu.write_byte(addr, val); },
-
-            // Window Y position
-            0x4A => { self.gpu.write_byte(addr, val); },
-
-            // Window X position
-            0x4B => { self.gpu.write_byte(addr, val); },
+            // GPU
+            0xFF40...0xFF4B => { self.gpu.write_byte(addr, val); },
 
             _ => {  }
         }
